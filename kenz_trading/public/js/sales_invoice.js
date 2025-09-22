6f8a7da29f1f84fca1fb35d95757bd42283d5360
Jishnu-Epics <jishnuvv2016@gmail.com>
2025-09-22 16:08:01 +0530

    feat:update default branch in invoice

--- kenz_trading/public/js/sales_invoice.js
+++ kenz_trading/public/js/sales_invoice.js
@@ -4,6 +4,7 @@ frappe.ui.form.on("Sales Invoice", {
     },
     customer: function (frm) {
         frm.set_value("custom_session_user", frappe.session.user);
+        default_branch(frm)
     },
 
 });
@@ -15,3 +16,37 @@ function set_pos_value(frm) {
         frm.set_value("is_pos", 1);
     }
 }
+
+function default_branch(frm) {
+    frappe.call({
+        method: "kenz_trading.events.sales_invoice.get_default_branch",
+        args: {
+            user: frappe.session.user,
+            
+        },
+        callback: function (r) {
+            if (r.message) {
+                frm.set_value("branch", r.message);
+            }
+        },
+    });
+}
+
+
+
+// frappe.ui.form.on("Sales Invoice Item", {
+//     item_code: function(frm, cdt, cdn) {
+//         var row = locals[cdt][cdn];
+//         if (row.item_code) {
+//             frm.set_query("uom", cdt, function() {
+//                 return {
+//                     query: "kenz_trading.events.item.get_item_uoms",
+//                     filters: {
+//                         "item_code": row.item_code
+//                     }
+//                 };
+//             });
+//         }
+//         console.log("UOM filter applied for item:", row.item_code);
+//     },
+// });
