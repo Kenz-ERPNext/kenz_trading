0fd1dde1432afa6e92b15c11712bd1a37f6c746c
Jishnu-Epics <jishnuvv2016@gmail.com>
2025-09-27 12:39:23 +0530

    feat:stock details add in sales invoice

--- kenz_trading/events/sales_invoice.py
+++ kenz_trading/events/sales_invoice.py
@@ -4,3 +4,9 @@ import frappe
 def get_default_branch(user):
     branch = frappe.db.get_value("User Permission", {"user":user,"allow":"Branch"}, "for_value")
     return branch if branch else None
+
+
+@frappe.whitelist()
+def get_uoms(item_code):
+    uoms = frappe.get_all("UOM Conversion Detail", filters={"parent": item_code}, fields=["uom", "conversion_factor"])
+    return uoms
