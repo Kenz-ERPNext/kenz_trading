fcccd6d88f0f968091360700c6abe8a83ae7e911
keerthana <keerthanachandrank@Keerthanas-MacBook-Air.local>
2025-10-16 14:49:18 +0530

    fix : set default value in tax category

--- kenz_trading/public/js/item.js
+++ kenz_trading/public/js/item.js
@@ -1,22 +1,30 @@
 frappe.ui.form.on('Item', {
-  refresh(frm) {
-    frappe.db.get_value('Branch', { custom_is_default: 1 }, 'name')
-      .then(r => {
-        if (r && r.message && r.message.name) {
-          frm.clear_table('custom_branches');
+  async onload_post_render(frm) {
+    if (frm.is_new()) {
+      if (!frm.doc.custom_branches?.length) {
+        const branchRes = await frappe.db.get_value('Branch', { custom_is_default: 1 }, 'name');
+        if (branchRes.message && branchRes.message.name) {
           let row = frm.add_child('custom_branches');
-          row.branch = r.message.name;
+          row.branch = branchRes.message.name;
           frm.refresh_field('custom_branches');
         }
-      });
-    frappe.db.get_value('Item Tax Template', { custom_is_default: 1 }, 'name')
-    .then(r => {
-      if (r && r.message && r.message.name) {
-        frm.clear_table('taxes');
-        let row = frm.add_child('taxes');
-        row.item_tax_template = r.message.name;
-        frm.refresh_field('taxes');
       }
-    });
+      if (!frm.doc.taxes?.length) {
+        const [taxTemplateRes, taxCategoryRes] = await Promise.all([
+          frappe.db.get_value('Item Tax Template', { custom_is_default: 1 }, 'name'),
+          frappe.db.get_value('Tax Category', { custom_is_default: 1 }, 'name')
+        ]);
+
+        const tax_template = taxTemplateRes?.message?.name;
+        const tax_category = taxCategoryRes?.message?.name;
+
+        if (tax_template || tax_category) {
+          let row = frm.add_child('taxes');
+          if (tax_template) row.item_tax_template = tax_template;
+          if (tax_category) row.tax_category = tax_category;
+          frm.refresh_field('taxes');
+        }
+      }
+    }
   }
 });
