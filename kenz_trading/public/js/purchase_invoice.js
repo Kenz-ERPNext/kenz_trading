cfbc27a755f81d880ef9889c21b10e38f1bbea89
frappe <sreejithcpattuvam.com>
2026-03-13 12:45:54 +0530

    pi status issue

--- kenz_trading/public/js/purchase_invoice.js
+++ kenz_trading/public/js/purchase_invoice.js
@@ -1,11 +1,20 @@
+
+
+
 // frappe.ui.form.on('Purchase Invoice', {
+
+//   refresh(frm){
+//         // frappe.msgprint("hiii")
+//     },
+
 //     onload(frm) {
+//         // Set default warehouse if update_stock is checked
 //         if (frm.doc.update_stock) {
 //             frappe.call({
 //                 method: "frappe.client.get_list",
 //                 args: {
 //                     doctype: "Warehouse",
-//                     filters: { "custom_is_default": 1},
+//                     filters: { "custom_is_default": 1 },
 //                     fields: ["name"],
 //                     limit_page_length: 1
 //                 },
@@ -14,7 +23,7 @@
 //                         frm.set_value('set_warehouse', r.message[0].name);
 //                         frm.refresh_field('set_warehouse');
 //                         frappe.show_alert({
-//                             message: 'Warehouse set to: ' + r.message.name,
+//                             message: 'Warehouse set to: ' + r.message[0].name,
 //                             indicator: 'green'
 //                         });
 //                     } else {
@@ -26,12 +35,29 @@
 //             frm.set_value('set_warehouse', '');
 //             frm.refresh_field('set_warehouse');
 //         }
+
+//         // Set paid_amount if invoice is marked as paid
+//         if (frm.doc.is_paid && (!frm.doc.paid_amount || frm.doc.paid_amount == 0)) {
+//             frm.set_value('paid_amount', frm.doc.rounded_total || 0);
+//             frm.refresh_field('paid_amount');
+//         }
 //     }
 // });
 
 
 
+
+
+
+
+
 frappe.ui.form.on('Purchase Invoice', {
+
+    refresh(frm) {
+
+
+    },
+
     onload(frm) {
         // Set default warehouse if update_stock is checked
         if (frm.doc.update_stock) {
@@ -46,25 +72,26 @@ frappe.ui.form.on('Purchase Invoice', {
                 callback: function(r) {
                     if (r.message && r.message.length > 0) {
                         frm.set_value('set_warehouse', r.message[0].name);
-                        frm.refresh_field('set_warehouse');
-                        frappe.show_alert({
-                            message: 'Warehouse set to: ' + r.message[0].name,
-                            indicator: 'green'
-                        });
-                    } else {
-                        frappe.msgprint(__('Warehouse "Stores - A" not found.'));
                     }
                 }
             });
         } else {
             frm.set_value('set_warehouse', '');
-            frm.refresh_field('set_warehouse');
+        }
+    },
+
+    validate(frm) {
+        // Set paid_amount only when saving
+        if (frm.doc.is_paid && (!frm.doc.paid_amount || frm.doc.paid_amount == 0)) {
+            frm.set_value('paid_amount', frm.doc.rounded_total || 0);
         }
 
-        // Set paid_amount if invoice is marked as paid
-        if (frm.doc.is_paid) {
-            frm.set_value('paid_amount', frm.doc.rounded_total || 0);
-            frm.refresh_field('paid_amount');
-        }
+        
     }
+
 });
+
+
+
+
+
--- kenz_trading/public/js/purchase_invoice_list.js
+++ kenz_trading/public/js/purchase_invoice_list.js
@@ -1,81 +1,82 @@
+
+
+
 // frappe.listview_settings["Purchase Invoice"] = {
+
+
+
+    
 //     onload: function(listview) {
 
-//         frappe.call({
-//             method: "frappe.client.get_value",
-//             args: {
-//                 doctype: "Kenza Settings",
-//                 fieldname: "enable_bulk_edit"
-//             },
-//             callback: function(r) {
+//         frappe.db.get_single_value("Kenza Settings", "enable_bulk_edit").then(value => {
 
-//                 if (r.message && r.message.enable_bulk_edit) {
+//             if (value) {
 
-//                     listview.page.add_inner_button("Bulk Print Preview", function() {
+//                 listview.page.add_inner_button("Bulk Print Preview", function() {
 
-//                         let d = new frappe.ui.Dialog({
-//                             title: "Select Print Format",
-//                             fields: [
-//                                 {
-//                                     label: "Print Format",
-//                                     fieldname: "print_format",
-//                                     fieldtype: "Link",
-//                                     options: "Print Format",
-//                                     reqd: 1,
-//                                     get_query: function () {
-//                                         return {
-//                                             filters: {
-//                                                 doc_type: "Purchase Invoice"
-//                                             }
-//                                         };
+//                     let d = new frappe.ui.Dialog({
+//                         title: "Select Print Format",
+//                         fields: [
+//                             {
+//                                 label: "Print Format",
+//                                 fieldname: "print_format",
+//                                 fieldtype: "Link",
+//                                 options: "Print Format",
+//                                 reqd: 1,
+//                                 get_query: function () {
+//                                     return {
+//                                         filters: {
+//                                             doc_type: "Purchase Invoice"
+//                                         }
+//                                     };
+//                                 }
+//                             }
+//                         ],
+//                         primary_action_label: "Print",
+//                         primary_action(values) {
+
+//                             let selected_format = values.print_format;
+
+//                             d.hide();
+
+//                             let filters = listview.get_filters_for_args();
+
+//                             frappe.call({
+//                                 method: "kenz_trading.api.bulk_print.get_purchase_invoice_names",
+//                                 args: { filters: filters },
+//                                 freeze: true,
+//                                 freeze_message: __("Preparing Combined PDF... Please wait..."),
+//                                 callback: function(res) {
+
+//                                     if (res.message && res.message.length) {
+
+//                                         let names = res.message;
+
+//                                         window.open(
+//                                             frappe.urllib.get_full_url(
+//                                                 "/api/method/frappe.utils.print_format.download_multi_pdf?"
+//                                                 + "doctype=Purchase Invoice"
+//                                                 + "&name=" + JSON.stringify(names)
+//                                                 + "&format=" + selected_format
+//                                                 + "&no_letterhead=0"
+//                                             )
+//                                         );
+
+//                                     } else {
+//                                         frappe.msgprint("No Submitted Purchase Invoices found.");
 //                                     }
 //                                 }
-//                             ],
-//                             primary_action_label: "Print",
-//                             primary_action(values) {
+//                             });
 
-//                                 let selected_format = values.print_format;
-
-//                                 d.hide();
-
-//                                 let filters = listview.get_filters_for_args();
-
-//                                 frappe.call({
-//                                     method: "kenz_trading.api.bulk_print.get_purchase_invoice_names",
-//                                     args: { filters: filters },
-//                                     freeze: true,
-//                                     freeze_message: __("Preparing Combined PDF... Please wait..."),
-//                                     callback: function(res) {
-
-//                                         if (res.message && res.message.length) {
-
-//                                             let names = res.message;
-
-//                                             window.open(
-//                                                 frappe.urllib.get_full_url(
-//                                                     "/api/method/frappe.utils.print_format.download_multi_pdf?"
-//                                                     + "doctype=Purchase Invoice"
-//                                                     + "&name=" + JSON.stringify(names)
-//                                                     + "&format=" + selected_format
-//                                                     + "&no_letterhead=0"
-//                                                 )
-//                                             );
-
-//                                         } else {
-//                                             frappe.msgprint("No Submitted Purchase Invoices found.");
-//                                         }
-//                                     }
-//                                 });
-
-//                             }
-//                         });
-
-//                         d.show();
-
+//                         }
 //                     });
 
-//                 }
+//                     d.show();
+
+//                 });
+
 //             }
+
 //         });
 
 //     }
@@ -83,79 +84,83 @@
 
 
 
-frappe.listview_settings["Purchase Invoice"] = {
-    onload: function(listview) {
 
-        frappe.db.get_single_value("Kenza Settings", "enable_bulk_edit").then(value => {
 
-            if (value) {
 
-                listview.page.add_inner_button("Bulk Print Preview", function() {
+if (!frappe.listview_settings["Purchase Invoice"]) {
+    frappe.listview_settings["Purchase Invoice"] = {};
+}
 
-                    let d = new frappe.ui.Dialog({
-                        title: "Select Print Format",
-                        fields: [
-                            {
-                                label: "Print Format",
-                                fieldname: "print_format",
-                                fieldtype: "Link",
-                                options: "Print Format",
-                                reqd: 1,
-                                get_query: function () {
-                                    return {
-                                        filters: {
-                                            doc_type: "Purchase Invoice"
-                                        }
-                                    };
+frappe.listview_settings["Purchase Invoice"].onload = function(listview) {
+
+    frappe.db.get_single_value("Kenza Settings", "enable_bulk_edit").then(value => {
+
+        if (value) {
+
+            listview.page.add_inner_button("Bulk Print Preview", function() {
+
+                let d = new frappe.ui.Dialog({
+                    title: "Select Print Format",
+                    fields: [
+                        {
+                            label: "Print Format",
+                            fieldname: "print_format",
+                            fieldtype: "Link",
+                            options: "Print Format",
+                            reqd: 1,
+                            get_query: function () {
+                                return {
+                                    filters: {
+                                        doc_type: "Purchase Invoice"
+                                    }
+                                };
+                            }
+                        }
+                    ],
+                    primary_action_label: "Print",
+                    primary_action(values) {
+
+                        let selected_format = values.print_format;
+                        d.hide();
+
+                        let filters = listview.get_filters_for_args();
+
+                        frappe.call({
+                            method: "kenz_trading.api.bulk_print.get_purchase_invoice_names",
+                            args: { filters: filters },
+                            freeze: true,
+                            freeze_message: __("Preparing Combined PDF... Please wait..."),
+                            callback: function(res) {
+
+                                if (res.message && res.message.length) {
+
+                                    let names = res.message;
+
+                                    window.open(
+                                        frappe.urllib.get_full_url(
+                                            "/api/method/frappe.utils.print_format.download_multi_pdf?"
+                                            + "doctype=Purchase Invoice"
+                                            + "&name=" + JSON.stringify(names)
+                                            + "&format=" + selected_format
+                                            + "&no_letterhead=0"
+                                        )
+                                    );
+
+                                } else {
+                                    frappe.msgprint("No Submitted Purchase Invoices found.");
                                 }
                             }
-                        ],
-                        primary_action_label: "Print",
-                        primary_action(values) {
+                        });
 
-                            let selected_format = values.print_format;
-
-                            d.hide();
-
-                            let filters = listview.get_filters_for_args();
-
-                            frappe.call({
-                                method: "kenz_trading.api.bulk_print.get_purchase_invoice_names",
-                                args: { filters: filters },
-                                freeze: true,
-                                freeze_message: __("Preparing Combined PDF... Please wait..."),
-                                callback: function(res) {
-
-                                    if (res.message && res.message.length) {
-
-                                        let names = res.message;
-
-                                        window.open(
-                                            frappe.urllib.get_full_url(
-                                                "/api/method/frappe.utils.print_format.download_multi_pdf?"
-                                                + "doctype=Purchase Invoice"
-                                                + "&name=" + JSON.stringify(names)
-                                                + "&format=" + selected_format
-                                                + "&no_letterhead=0"
-                                            )
-                                        );
-
-                                    } else {
-                                        frappe.msgprint("No Submitted Purchase Invoices found.");
-                                    }
-                                }
-                            });
-
-                        }
-                    });
-
-                    d.show();
-
+                    }
                 });
 
-            }
+                d.show();
 
-        });
+            });
 
-    }
+        }
+
+    });
+
 };
