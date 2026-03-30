1f0606e964fbce38d373577d788cafa5356ec55c
frappe <sreejithcpattuvam.com>
2026-03-30 14:49:10 +0530

    tin

--- kenz_trading/kenz_trading/doctype/kenza_settings/kenza_settings.js
+++ kenz_trading/kenz_trading/doctype/kenza_settings/kenza_settings.js
@@ -21,7 +21,7 @@ frappe.ui.form.on("Kenza Settings", {
                     }
                 });
            
-    },
+    }, 
 
     create_tax_template: function(frm) {
 
@@ -52,4 +52,29 @@ frappe.ui.form.on("Kenza Settings", {
                 });
            
     },
+
+    clear_tin_in_customer: function(frm) {
+
+        frappe.confirm(
+            "Are you sure you want to CLEAR TIN values from all Customers?",
+            function() {
+
+                cur_frm.call({
+                    doc: cur_frm.doc,
+                    method: "tin_clear",
+                    args: {},
+                    freeze: true,
+                    freeze_message: "Clearing TIN values...",
+
+                    callback: function(r) {
+                        if (r.message) {
+                            frappe.msgprint("✅ TIN Values Cleared Successfully");
+                        }
+                    }
+                });
+
+            }
+        );
+
+    }
 }); 
