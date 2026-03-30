6a721818f4f799dee0d100a7e3c57fc0cbf71a93
frappe <sreejithcpattuvam.com>
2026-03-30 11:39:11 +0530

    remove TIN

--- kenz_trading/public/js/customer.js
+++ kenz_trading/public/js/customer.js
@@ -24,29 +24,30 @@
 
 
 frappe.ui.form.on("Customer", {
-    custom_vat_registration_number: function (frm) {
-        let vat = frm.doc.custom_vat_registration_number;
-        if (vat) {
-            // 1️⃣ Set tax_id
-            frm.set_value("tax_id", vat);
+    
+    // custom_vat_registration_number: function (frm) {
+    //     let vat = frm.doc.custom_vat_registration_number;
+    //     if (vat) {
+    //         // 1️⃣ Set tax_id
+    //         frm.set_value("tax_id", vat);
 
-            // 2️⃣ Update / Insert TIN in custom_additional_ids
-            let found = false;
-            (frm.doc.custom_additional_ids || []).forEach(row => {
-                if (row.type_name === "Tax Identification Number" && row.type_code === "TIN") {
-                    row.value = vat;
-                    found = true;
-                }
-            });
-            if (!found) {
-                let row = frm.add_child("custom_additional_ids");
-                row.type_name = "Tax Identification Number";
-                row.type_code = "TIN";
-                row.value = vat;
-            }
-        }
-        frm.refresh_field("custom_additional_ids");
-    },
+    //         // 2️⃣ Update / Insert TIN in custom_additional_ids
+    //         let found = false;
+    //         (frm.doc.custom_additional_ids || []).forEach(row => {
+    //             if (row.type_name === "Tax Identification Number" && row.type_code === "TIN") {
+    //                 row.value = vat;
+    //                 found = true;
+    //             }
+    //         });
+    //         if (!found) {
+    //             let row = frm.add_child("custom_additional_ids");
+    //             row.type_name = "Tax Identification Number";
+    //             row.type_code = "TIN";
+    //             row.value = vat;
+    //         }
+    //     }
+    //     frm.refresh_field("custom_additional_ids");
+    // },
 
     custom_cr_no: function (frm) {
         let cr = frm.doc.custom_cr_no;
