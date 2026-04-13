31dd87098d10a710f8256df2bb75510b78843811
frappe <sreejithcpattuvam.com>
2026-04-13 09:53:16 +0530

    up

--- kenz_trading/public/js/payment_entry.js
+++ kenz_trading/public/js/payment_entry.js
@@ -1,15 +1,15 @@
 frappe.ui.form.on("Payment Entry", {
-    refresh(frm) {
-        if (frm.doc.mode_of_payment === "Cash") {
-            frm.set_value("reference_no", "Cash");
-            frm.set_value("reference_date", frappe.datetime.get_today());
-        } 
-    },
+    // refresh(frm) {
+    //     if (frm.doc.mode_of_payment === "Cash") {
+    //         frm.set_value("reference_no", "Cash");
+    //         frm.set_value("reference_date", frappe.datetime.get_today());
+    //     } 
+    // },
 
-    mode_of_payment(frm) {
-        if (frm.doc.mode_of_payment === "Cash") {
-            frm.set_value("reference_no", "Cash");
-            frm.set_value("reference_date", frappe.datetime.get_today());
-        } 
-    }
+    // mode_of_payment(frm) {
+    //     if (frm.doc.mode_of_payment === "Cash") {
+    //         frm.set_value("reference_no", "Cash");
+    //         frm.set_value("reference_date", frappe.datetime.get_today());
+    //     } 
+    // }
 });
